1a63fb1e71a2290f5e1e58ceffd50dc788b5e7a6
Pablo J. Dols <hola@pablodols.com>
2020-09-10 18:11:12 +0200

    Archivo recibido de fiverr

--- public/wp-content/themes/html5blank-stable/archive-lugar.php
+++ public/wp-content/themes/html5blank-stable/archive-lugar.php
@@ -27,12 +27,15 @@
           <p><strong>Latitud:</strong> <?php the_field('lat') ?></p>
           <p><strong>Longitud:</strong> <?php the_field('lon') ?></p>
           
+         
           
         </div>
       </div>
         <?php $counter++; ?>
       <?php endwhile; wp_reset_postdata(); ?>
     </div>
+  </div>
+  <div id="map">
   </div>
 </section>
 
@@ -41,82 +44,142 @@
     #map,
     #acf-map { height: 500px; }
 </style>
-<div id="map" style="width: 600px;">
+ <?php
+          $arg = array( 'post_type' => 'lugar', 'posts_per_page' => 10,);
+          $loop = new WP_Query( $arg );
+//           $array = array();
+//           while ( $loop->have_posts() ) : $loop->the_post();
+//           global $places;
+//           $array[] = array(
+//           'subtitle' => the_field('subtitle'),
+//           'address' => the_field('address'),
+//           'category'=> the_field('category'),
+//           'Latitude'=> the_field('lat'),
+//           'Longitude'=> the_field('lon'),
+//           );
+// 
+// endwhile;
+// 
+// wp_reset_query();
+// ob_clean();
+//  echo wp_json_encode($array);
+ $posts = $loop ->get_posts();
+ foreach( $posts as $post ) { 
+$output[] = array(
+
+          'address' => $post->address,
+          'category'=> $post->category,
+          'Latitude'=> $post->lat,
+          'Longitude'=>$post->lon
+);
+ } 
+$json =json_encode($output);
+?>
+
+<script
+        src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/leaflet.markercluster.js">  </script>
+         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.Default.css">
+
+ 
 
 
-</div>
-<div class="marker" data-lat="<?php echo the_field('lat') ?>" data-lng="<?php echo the_field('lon'); ?>" data-title="<?php echo the_field('subtitle')  ?>" data-url="<?php echo 'link'; ?>" data-category="<?php echo 'category'; ?>"></div>
+  
+  
+  
 
+    <script>
+   
+        var data = <?php echo $json ?>;
+        // Create map instance
+        var map = L.map('map',{
+            center:[38.840401, 0.208840],
+            zoom:10
+        });
 
-<script>
-$( document ).ready(function() {
-  var map = L.map('map').setView([39.8410802, -0.1191397], 8);
+          L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+maxZoom: 18,
+id: 'mapbox/streets-v11',
+tileSize: 512,
+zoomOffset: -1,
+accessToken: 'pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWkifQ.9rqymFnsW79aCkAFGCo0XQ'
+}).addTo(map);
+
     
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox/streets-v11',
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken: 'pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWkifQ.9rqymFnsW79aCkAFGCo0XQ'
-          }).addTo(map); 
+
+        //create markerclustergroup 
+        var markers=L.markerClusterGroup();
+        // create a geojson object
+        let geojsonData = createGeoJson(data);
+
+        function createGeoJson(data) {
+            var geojson = {
+                "type":"FeatureCollection",
+                "features":[
+
+                ]
+            };
+
+            // iterate through the data array
+            data.forEach(element => {
+                let marker = L.marker([element.Latitude, element.Longitude]);
+                let pntGeojson = marker.toGeoJSON();
+
+                pntGeojson.properties = element;
+                geojson.features.push(pntGeojson);
+            });
+
+            return geojson;
+        }
 
 
-var marker = [];
-$.each($('.marker'), function () {
-  var lat = $(this).attr('data-lat');
-  var lng = $(this).attr('data-lng');
-  var name = $(this).attr('data-title');
-  var link = $(this).attr('data-url');
-  console.log( 'Marker es: ' + marker );
-  marker = [ name, lat, lng, link ];
-  console.log( 'Y ahora es: ' + marker );
-  console.log( marker );
-});
+        // create a  geojson instance
+        var categorynone = L.geoJson(geojsonData, {
+            onEachFeature:function(feature, layer) {
+                let content ="<table class='table table-striped table-bordered table-condensed'>" +
+                    "<tr><th>address</th><td>" + feature.properties.address + "</td></tr>" + "<tr><th>category</th><td>" + feature.properties.category +"</td></tr><table>";
+                
+                layer.bindPopup(content);
+            },
+            pointToLayer:function(geoObj, latLng) {
+                return L.marker(latLng);
+              
+            },
+            filter:function (feature,layer){
+                return feature.properties.category == "";  
+            }
 
-var markers = [ marker ];
-console.log(markers);
-console.log(markers.length);
-for (var i = 0; i < markers.length; i++) {
-  marker = new L.marker([markers[i][1],markers[i][2]])
-      .bindPopup(markers[i][0])
-      .addTo(map);
-}
-});
+        });
+        var category1 = L.geoJson(geojsonData, {
+            onEachFeature:function(feature, layer) {
+                let content ="<table class='table table-striped table-bordered table-condensed'>" +
+                    "<tr><th>address</th><td>" + feature.properties.address + "</td></tr>" + "<tr><th>category</th><td>" + feature.properties.category +"</td></tr><table>";
+                
+                layer.bindPopup(content);
+            },
+            pointToLayer:function(geoObj, latLng) {
+                return L.marker(latLng);
+              
+            },
+            filter:function (feature,layer){
+                return feature.properties.category == "categoria1";  
+            }
 
-
-</script>
-
-<script>
-
-
-
-// var markerList = [
-//   ["A",39.8410802,-0.1191397],
-//   ["B",39.8410802,-0.1291397],
-//   ["C",39.8410802,-0.1391397],
-//   ["D",39.8510802,-0.1191397],
-//   ["E",39.8610802,-0.1291397],
-//   ["F",39.8710802,-0.1391397],
-
-//   ];
-
-//       var map = L.map('map').setView([39.8410802, -0.1191397], 12);
-//       mapLink = 
-//           '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-//           L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//           maxZoom: 18,
-//           id: 'mapbox/streets-v11',
-//           tileSize: 512,
-//           zoomOffset: -1,
-//           accessToken: 'pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWkifQ.9rqymFnsW79aCkAFGCo0XQ'
-//           }).addTo(map);
-
-//   for (var i = 0; i < markerList.length; i++) {
-//     marker = new L.marker([markerList[i][1],markerList[i][2]])
-//       .bindPopup(markerList[i][0])
-//       .addTo(map);
-//   }
-             
-  </script>
+        });
+        
+         
+          
+        var all = L.layerGroup([category1,categorynone]);
+        L.control.layers({
+            "All":all,
+            "categorynone":categorynone,
+            "category1":category1,
+            "cluster":markers,
+        }).addTo(map);
+        markers.addLayer(all);
+        markers.addTo(map);
+    </script>
