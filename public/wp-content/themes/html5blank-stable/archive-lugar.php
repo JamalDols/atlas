1ff13fd320e92935a3f9dbddac8b32b2e364230c
Pablo J. Dols <hola@pablodols.com>
2020-09-30 14:43:37 +0200

    Test popup en click, quito  layer.bindPopup(content);

--- public/wp-content/themes/html5blank-stable/archive-lugar.php
+++ public/wp-content/themes/html5blank-stable/archive-lugar.php
@@ -1,7 +1,13 @@
 <?php /* Template Name: Demo Page Template */ get_header(); ?>
 <h1 class="main-title"><?= post_type_archive_title(); ?></h1>
 
-
+<div aria-labelledby="myModalLabel" class="modal left fade" id="emptymodal" role="dialog" tabindex="-1">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content"></div>
+      <!-- modal-content -->
+    </div>
+    <!-- modal-dialog -->
+  </div>
 
 
 
@@ -47,22 +53,6 @@
  <?php
           $arg = array( 'post_type' => 'lugar', 'posts_per_page' => 10,);
           $loop = new WP_Query( $arg );
-//           $array = array();
-//           while ( $loop->have_posts() ) : $loop->the_post();
-//           global $places;
-//           $array[] = array(
-//           'subtitle' => the_field('subtitle'),
-//           'address' => the_field('address'),
-//           'category'=> the_field('category'),
-//           'Latitude'=> the_field('lat'),
-//           'Longitude'=> the_field('lon'),
-//           );
-// 
-// endwhile;
-// 
-// wp_reset_query();
-// ob_clean();
-//  echo wp_json_encode($array);
  $posts = $loop ->get_posts();
  foreach( $posts as $post ) { 
 $output[] = array(
@@ -83,6 +73,7 @@ $json =json_encode($output);
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/leaflet.markercluster.js">  </script>
          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.Default.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
  
 
@@ -92,6 +83,12 @@ $json =json_encode($output);
   
 
     <script>
+PlaceMarker = L.CircleMarker.extend({
+  options: {
+    id: ''
+  }
+});
+
    
         var data = <?php echo $json ?>;
         // Create map instance
@@ -143,7 +140,7 @@ accessToken: 'pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWk
                 let content ="<table class='table table-striped table-bordered table-condensed'>" +
                     "<tr><th>Dirección:</th><td>" + feature.properties.address + "</td></tr>" + "<tr><th>Categoría: </th><td>" + feature.properties.category +"</td></tr><table>";
                 
-                layer.bindPopup(content);
+               
             },
             pointToLayer:function(geoObj, latLng) {
                 return L.marker(latLng);
@@ -159,7 +156,7 @@ accessToken: 'pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWk
                 let content ="<table class='table table-striped table-bordered table-condensed'>" +
                     "<tr><th>Dirección:</th><td>" + feature.properties.address + "</td></tr>" + "<tr><th>Categoría: </th><td>" + feature.properties.category +"</td></tr><table>";
                 
-                layer.bindPopup(content);
+             
             },
             pointToLayer:function(geoObj, latLng) {
                 return L.marker(latLng);
@@ -175,8 +172,7 @@ accessToken: 'pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWk
             onEachFeature:function(feature, layer) {
                 let content ="<table class='table table-striped table-bordered table-condensed'>" +
                     "<tr><th>Dirección:</th><td>" + feature.properties.address + "</td></tr>" + "<tr><th>Categoría: </th><td>" + feature.properties.category +"</td></tr><table>";
-                
-                layer.bindPopup(content);
+          
             },
             pointToLayer:function(geoObj, latLng) {
                 return L.marker(latLng);
@@ -203,4 +199,107 @@ accessToken: 'pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWk
 
         console.log(markers);
 
+//TODO: Ahora solo falta recuperar la información y sacarla por aquí
+markers.on('click', function (e) {
+        console.log('show');
+        var id = this.options.id;
+        $(".modal-content").html('Marker id: ' + id);
+        $('#emptymodal').modal('show');
+        map.setView(e.target.getLatLng());
+    });
+
+    /*Close modal on map click */
+    map.on('click', function (e) {
+        console.log('Hide');
+        // $('.modal').modal('hide');
+    });
+
     </script>
+<style>
+    /*******************************
+    * MODAL AS LEFT/RIGHT SIDEBAR
+    * Add "left" or "right" in modal parent div, after class="modal".
+    * Get free snippets on bootpen.com
+    *******************************/
+
+    .modal.left .modal-dialog,
+    .modal.right .modal-dialog {
+    position: fixed;
+    margin: auto;
+    width: 30%;
+    height: 100%;
+    -webkit-transform: translate3d(0%, 0, 0);
+    -ms-transform: translate3d(0%, 0, 0);
+    -o-transform: translate3d(0%, 0, 0);
+    transform: translate3d(0%, 0, 0);
+    }
+
+    .modal.left .modal-content,
+    .modal.right .modal-content {
+    height: 100%;
+    overflow-y: auto;
+    }
+
+    .modal.left .modal-body,
+    .modal.right .modal-body {
+    padding: 15px 15px 80px;
+    }
+
+
+    /*Left*/
+
+    .modal.left.fade .modal-dialog {
+    left: 0px;
+    -webkit-transition: opacity 0.3s linear, left 0.3s ease-out;
+    -moz-transition: opacity 0.3s linear, left 0.3s ease-out;
+    -o-transition: opacity 0.3s linear, left 0.3s ease-out;
+    transition: opacity 0.3s linear, left 0.3s ease-out;
+    }
+
+    .modal.left.fade.in .modal-dialog {
+    left: 0;
+    }
+
+
+    /*Right*/
+
+    .modal.right.fade .modal-dialog {
+    right: -320px;
+    -webkit-transition: opacity 0.3s linear, right 0.3s ease-out;
+    -moz-transition: opacity 0.3s linear, right 0.3s ease-out;
+    -o-transition: opacity 0.3s linear, right 0.3s ease-out;
+    transition: opacity 0.3s linear, right 0.3s ease-out;
+    }
+
+    .modal.right.fade.in .modal-dialog {
+    right: 0;
+    }
+
+
+    /* ----- MODAL STYLE ----- */
+
+    .modal-content {
+    border-radius: 0;
+    border: none;
+    }
+
+    .modal-header {
+    border-bottom-color: #EEEEEE;
+    background-color: #FAFAFA;
+    }
+
+    .modal-backdrop {
+    background-color: transparent;
+    display: none;
+    }
+
+    .modal {
+    pointer-events: none;
+    z-index: 10000000000000;
+    }
+
+    .modal-content {
+    pointer-events: auto;
+    }
+
+</style>
