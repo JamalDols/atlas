76825859503435289d4e5c747570d1adc66d0b51
Pablo J. Dols <hola@pablodols.com>
2020-10-14 14:33:13 +0200

    Mover lista de post a template-fonts

---
+++ public/wp-content/themes/atlas/template-fonts.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Fonts Template
+ */
+?>
+
+<section class="list-cases">
+  <div class="container">
+    <div class="row position-relative">
+      <?php
+        $args = array(
+          'post_type' => 'lugar',
+          'posts_per_page' => 10,
+        );
+        $query = new WP_Query( $args );
+        ?>
+        
+        <?php $counter = 1; ?>
+      <?php while( $query->have_posts() ) : $query->the_post() ?>
+      <div class="item-case-<?= $counter ?> col-md-12" data-hover="<?= $counter ?>" data-aos="fade-up" data-aos-duration="2000">
+        <div class="case-container"> 
+          <?php the_title( '<h2 class="entry-title"><a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '" rel="bookmark">', '</a></h2>' ); ?>
+          <p><?php the_field('subtitle') ?></p>
+          <p><?php the_field('address') ?></p>
+          <p><strong>Latitud:</strong> <?php the_field('lat') ?></p>
+          <p><strong>Longitud:</strong> <?php the_field('lon') ?></p>
+          
+         
+          
+        </div>
+      </div>
+        <?php $counter++; ?>
+      <?php endwhile; wp_reset_postdata(); ?>
+    </div>
+  </div>
+  <div id="map">
+  </div>
+</section>
