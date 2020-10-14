f53956b3687f60beb65b350e65b7ea2cf287fb9e
Pablo J. Dols <hola@pablodols.com>
2020-10-14 14:32:34 +0200

    Update header.php

--- public/wp-content/themes/atlas/templates/header.php
+++ public/wp-content/themes/atlas/templates/header.php
@@ -1,6 +1,8 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+  <div class="container-fluid">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+      <img src="<?= get_template_directory_uri(); ?>/dist/images/logo-valencia-sostenible.svg" alt="">
+    </a>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
