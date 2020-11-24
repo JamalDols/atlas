aa827104356f224fb4540734bd0908cddc292787
Pablo J. Dols <hola@pablodols.com>
2020-11-24 15:46:14 +0100

    Update single-lugar.php

--- public/wp-content/themes/atlas/single-lugar.php
+++ public/wp-content/themes/atlas/single-lugar.php
@@ -1,24 +1,13 @@
-<?php /* Template Name: Demo Page Template */ get_header(); ?>
-
-<section class="page-header-section" data-aos="fade-up">
-
-<h1 class="main-title"><?= the_title()?></h1>
-
-
-</section>
-<?php while (have_posts()) : the_post(); 
-$counter = 1;
+<?php
+/**
+ * Template Name: Font Ajuntament
+ */
 ?>
 
-<div class="featured-image"
-style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>');width: 400px;
-height: 400px;
-background-size: cover;
-background-position: center;">
-</div>
+
 <?php
 //VARS
-$title = get_field('title'); 
+$title = get_the_title(); 
 $subtitle = get_field('subtitle'); 
 $address = get_field('address'); 
 $lat = get_field('lat'); 
@@ -28,26 +17,53 @@ $category = get_field('category');
 
 
 
-<div class="content"></div>
-<h1 class="title"><?php echo $title ?></h1>
-<h2 class="subtitle"><?php echo $subtitle ?></h2>
 
-<span class="address"><strong>Dirección:</strong><?php echo $address ?></span>
-<br> 
-<span class="category"><strong>Categoría:</strong><?php echo $category ?></span>
-<br> 
-<span class="lat"><strong>Longitud:</strong><?php echo $lat ?></span>
-<br> 
-<span class="lon"><strong>Latitud:</strong><?php echo $lon ?></span>
 
-<?php $counter++; ?>
 
+
+<?php while (have_posts()) : the_post(); ?>
+
+
+<section class="page-header">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1><?php echo $title ?></h1>
+        <span class="address"><?php echo $address ?></span>
+      </div>
+    </div>
+  </div>
+</section>
+
+  <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
-<style>
-#mapid { height: 500px; }
-</style>
-<div id="mapid"></div>
+
+<section class="single-place">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="text">
+          <p>La Plaça de l’Ajuntament és la plaça més gran i cèntrica de la ciutat de València. Ocupa els solars resultants de l’enderrocament del convent de Sant Francesc. Durant el franquisme rebia el nom de Plaza del Caudillo i després de la Transició democràtica espanyola, es va anomenar Plaça del País Valencià fins a l’any 1987. Entre 1936 i 1939, s’anomenava Plaça Emili Castelar. Hi ha una proposta per part del Consell Valencià de Cultura que reba el nom de Jaume I, el fundador del Regne de València. Després de les revoltes del 15 de Maig, els manifestants rebatejaren simbòlicament la plaça com a Plaça del Quinze de Maig, modificant les plaques per a mostrar aquest nou nom. Està declarada Conjunt Històric Artístic.</p>
+          <p>Té una forma triangular, amb la Plaça del Mercat al nord i la replaça de l’Estació del Nord (València) al sud. A la vora de l’oest hi trobem l’Ajuntament de València, i a l’est l’edifici de Correus. Al nord de la plaça hi ha una gran font, I al sud, un gran espai obert envoltat d’aparadors de flors. La seua fesomia actual procedeix de la reforma realitzada per l’arquitecte Javier Goerlich entre 1931 i 1935, tot I que ha desaparegut el mercat neobarroc de flors subterrani que va rebre el nom popular de La Escupidera.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="image" style="margin-bottom:30px;">
+        <div class="featured-image">
+            <?php if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+              } else { ?>
+              <img class="img-fluid" src="http://via.placeholder.com/850x500?text=No+Image" />
+            <?php } ?>
+        </div>
+        </div>
+        <div id="mapid"></div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <script>
 var L = window.L;
 var mymap = L.map('mapid').setView([<?php echo $lat ?>, <?php echo $lon ?>], 13);
@@ -76,7 +92,5 @@ popup
 
 mymap.on('click', onMapClick);
 </script>
-
-
 
 
