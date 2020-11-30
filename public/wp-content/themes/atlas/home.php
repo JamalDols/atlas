8d38c853f21daf4d82e17d00b12bd0882bafa371
Pablo J. Dols <hola@pablodols.com>
2020-11-30 13:33:07 +0100

    Cambiar posición botones mapa

--- public/wp-content/themes/atlas/home.php
+++ public/wp-content/themes/atlas/home.php
@@ -185,8 +185,12 @@ $json =json_encode($output);
         // Create map instance
         var map = L.map('map',{
             center:[39.4697500, -0.3773900],
-            zoom:14
+            zoom:14,
+            zoomControl: false
         });
+        L.control.zoom({
+          position: 'bottomright'
+        }).addTo(map);
 
           L.tileLayer('https://api.mapbox.com/styles/v1/jamaldols/ckg95x9iu77ew1apgpekvc9j3/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFtYWxkb2xzIiwiYSI6ImNrYmF2bGowOTBycGEyeG84b2F2NGlsYWkifQ.9rqymFnsW79aCkAFGCo0XQ', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
